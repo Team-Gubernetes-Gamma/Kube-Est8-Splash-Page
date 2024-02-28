94cfbb4903403ee093faa8b460dcf8788bfb8591
ppost0 <p.post2321@yahoo.com>
2024-02-27 22:22:03 -0500

    Change background color

--- app/page.js
+++ app/page.js
@@ -122,7 +122,7 @@ export default function Home() {
 
 
     {/* ---------------------------------MEET THE TEAM BLOCK------------------------------ */}
-  <section class="text-gray-400 bg-gray-900 body-font">
+  <section class="text-gray-400 body-font">
   <div class="container px-5 py-24 mx-auto">
     <div class="flex flex-col text-center w-full mb-20">
       <h1 class="text-2xl font-medium title-font mb-4 text-white">OUR TEAM</h1>
