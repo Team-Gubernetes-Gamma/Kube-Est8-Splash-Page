12c35312046e199470aeab60233367ace1b2da9f
erikawester <erikarwester@gmail.com>
2024-02-27 21:13:32 -0500

    add Team Members, GitHub, Linkedin. Fix spelling errors

--- app/page.js
+++ app/page.js
@@ -42,7 +42,7 @@ export default function Home() {
   <div class="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
     <div class="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
       <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">Kubernetes monitoring
-        <br class="hidden lg:inline-block"/>made simple.
+        <br class="hidden lg:inline-block"/> made simple.
       </h1>
       <p class="mb-8 leading-relaxed">Track the health of your cluster at various levels. Understand resource usage of each deployment object, detect and anticipate bottlenecks before they occur, and scan your cluster for security vulnerabilities.</p>
       <div class="flex justify-center">
@@ -70,8 +70,8 @@ export default function Home() {
         <div class="w-24 h-full bg-indigo-500"></div>
       </div>
       <div class="flex flex-wrap sm:flex-row flex-col py-6 mb-12">
-        <h1 class="sm:w-2/5 text-white font-medium title-font text-2xl mb-2 sm:mb-0">Space The Final Frontier</h1>
-        <p class="sm:w-3/5 leading-relaxed text-base sm:pl-10 pl-0">Street art subway tile salvia four dollar toast bitters selfies quinoa yuccie synth meditation iPhone intelligentsia prism tofu. Viral gochujang bitters dreamcatcher.</p>
+        <h1 class="sm:w-2/5 text-white font-medium title-font text-2xl mb-2 sm:mb-0">Meet the Team Behind KubeEst8</h1>
+        {/* <p class="sm:w-3/5 leading-relaxed text-base sm:pl-10 pl-0">Street art subway tile salvia four dollar toast bitters selfies quinoa yuccie synth meditation iPhone intelligentsia prism tofu. Viral gochujang bitters dreamcatcher.</p> */}
       </div>
     </div>
     <div class="flex flex-wrap sm:-m-4 -mx-4 -mb-10 -mt-4">
@@ -79,37 +79,53 @@ export default function Home() {
         <div class="rounded-lg h-64 overflow-hidden">
           <img alt="content" class="object-cover object-center h-full w-full" src="https://dummyimage.com/1203x503"/>
         </div>
-        <h2 class="text-xl font-medium title-font text-white mt-5">Shooting Stars</h2>
-        <p class="text-base leading-relaxed mt-2">Swag shoindxgoitch literally meditation subway tile tumblr cold-pressed. Gastropub street art beard dreamcatcher neutra, ethical XOXO lumbersexual.</p>
-        <a class="text-indigo-400 inline-flex items-center mt-3">Learn More
+        <h2 class="text-xl font-medium title-font text-white mt-5">Brandon Hernandez</h2>
+        <p class="text-base leading-relaxed mt-2"> GitHub</p>
+        <p class="text-base leading-relaxed mt-2"> Linkedin</p>
+        {/* <a class="text-indigo-400 inline-flex items-center mt-3">
           <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
             <path d="M5 12h14M12 5l7 7-7 7"></path>
           </svg>
-        </a>
+        </a> */}
       </div>
       <div class="p-4 md:w-1/3 sm:mb-0 mb-6">
         <div class="rounded-lg h-64 overflow-hidden">
           <img alt="content" class="object-cover object-center h-full w-full" src="https://dummyimage.com/1204x504"/>
         </div>
-        <h2 class="text-xl font-medium title-font text-white mt-5">The Catalyzer</h2>
-        <p class="text-base leading-relaxed mt-2">Swag shoindxigoitch literally meditation subway tile tumblr cold-pressed. Gastropub street art beard dreamcatcher neutra, ethical XOXO lumbersexual.</p>
-        <a class="text-indigo-400 inline-flex items-center mt-3">Learn More
+        <h2 class="text-xl font-medium title-font text-white mt-5">Erika Wester</h2>
+        <p class="text-base leading-relaxed mt-2"> GitHub</p>
+        <p class="text-base leading-relaxed mt-2"> Linkedin</p>
+        {/* <a class="text-indigo-400 inline-flex items-center mt-3">Learn More
           <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
             <path d="M5 12h14M12 5l7 7-7 7"></path>
           </svg>
-        </a>
+        </a> */}
       </div>
       <div class="p-4 md:w-1/3 sm:mb-0 mb-6">
         <div class="rounded-lg h-64 overflow-hidden">
           <img alt="content" class="object-cover object-center h-full w-full" src="https://dummyimage.com/1205x505"/>
         </div>
-        <h2 class="text-xl font-medium title-font text-white mt-5">The 400 Blows</h2>
-        <p class="text-base leading-relaxed mt-2">Swag shoindegoitch literally meditation subway tile tumblr cold-pressed. Gastropub street art beard dreamcatcher neutra, ethical XOXO lumbersexual.</p>
-        <a class="text-indigo-400 inline-flex items-center mt-3">Learn More
+        <h2 class="text-xl font-medium title-font text-white mt-5">Patrick Post</h2>
+        <p class="text-base leading-relaxed mt-2"> GitHub</p>
+        <p class="text-base leading-relaxed mt-2"> Linkedin</p>
+        {/* <a class="text-indigo-400 inline-flex items-center mt-3">Learn More
           <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
             <path d="M5 12h14M12 5l7 7-7 7"></path>
           </svg>
-        </a>
+        </a> */}
+      </div>
+      <div class="p-4 md:w-1/3 sm:mb-0 mb-6">
+        <div class="rounded-lg h-64 overflow-hidden">
+          <img alt="content" class="object-cover object-center h-full w-full" src="https://dummyimage.com/1205x505"/>
+        </div>
+        <h2 class="text-xl font-medium title-font text-white mt-5">Ines Chavez</h2>
+        <p class="text-base leading-relaxed mt-2"> GitHub</p>
+        <p class="text-base leading-relaxed mt-2"> Linkedin</p>
+        {/* <a class="text-indigo-400 inline-flex items-center mt-3">Learn More
+          <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
+            <path d="M5 12h14M12 5l7 7-7 7"></path>
+          </svg>
+        </a> */}
       </div>
     </div>
   </div>
