17aadaf07c0b42e443404d26e746357595f2c2b4
ppost0 <p.post2321@yahoo.com>
2024-02-28 00:01:43 -0500

    Fix profile links

--- app/page.js
+++ app/page.js
@@ -137,16 +137,11 @@ export default function Home() {
             <h3 class="text-gray-500 mb-3">Software Engineer</h3>
             <p class="mb-4">Patrick played a crucial role in both the front and back ends of our application. When he isn't coding, you can probably still find him on the computer playing games.</p>
             <span class="inline-flex">
-              <a class="text-gray-700">
+              <a href='https://www.linkedin.com/in/patrickppost/' class="text-gray-700">
                 <Image src='/linkedin.svg' width={24} height={24}/>
               </a>
-              <a class="ml-2 text-gray-700">
+              <a href='https://github.com/ppost0' class="ml-2 text-gray-700">
                 <Image src='/github.svg' width={24} height={24}/>
-              </a>
-              <a class="ml-2 text-gray-700">
-                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
-                  <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
-                </svg>
               </a>
             </span>
           </div>
